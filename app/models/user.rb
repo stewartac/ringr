42846f0a591d0cb283d9c72535d6c34f758b3916
Alexander Stewart <ac_stewart@icloud.com>
2020-03-02 17:00:10 +0000

    bookings model created and has_many relationships confirmed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :teams
+  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_165427) do
+ActiveRecord::Schema.define(version: 2020_03_02_165725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_bookings_on_game_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "address"
@@ -57,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_165427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "games"
+  add_foreign_key "bookings", "users"
   add_foreign_key "games", "teams"
   add_foreign_key "teams", "users", column: "captain_id"
 end
