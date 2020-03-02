e5d8059100a06a784cb9bec920ae7c5f227cd3a7
Alexander Stewart <ac_stewart@icloud.com>
2020-03-02 17:57:54 +0000

    added core routes and status to bookings table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :games do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :users, only: [:show, :edit, :update] do
+    resources :teams, except: [:index]
+  end
+
+  resources :bookings, only: [:show]
+
 end
