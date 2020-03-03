d20a7bc6d27560c734e6169e514ece4faf34998e
Henry Gunn <gunn26@hotmail.co.uk>
2020-03-03 14:49:47 +0000

    added update method to teams controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,45 @@
+class TeamsController < ApplicationController
+
+  before_action :set_team, :only [:show, :edit :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.create(team_params)
+    @team.user = current_user
+    if @team.save
+      redirect_to user_team_path(@team)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @team.update(title: params[:team][:title], league_link: params[:team][:league_link])
+    redirect_to user_team_path(@team)
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to user_path(@team)
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name, :league_link)
+  end
+
+end
