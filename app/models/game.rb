efdc4b32dfe1354f4b7d72561a2d44c13ca09d7c
Theodore Verge <theodore.verge@gmail.com>
2020-03-04 13:55:04 +0000

    search bar shows within homepage

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,5 @@
 class Game < ApplicationRecord
   searchkick
-
-  #    include PgSearch::Model
-  # pg_search_scope :search_by_address,
-  #   against: [:address],
-  #   using: {
-  #     tsearch: { prefix: true }
-  #   }
-
   belongs_to :team
   has_many :bookings
   has_many :reviews
