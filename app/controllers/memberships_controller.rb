095add7f50f28cf2841eb1154ab3ef14f3855818
Alexander Stewart <ac_stewart@icloud.com>
2020-03-09 18:31:58 +0000

    initial crud working - further logic required

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -5,11 +5,21 @@ class MembershipsController < ApplicationController
   end
 
   def create
-    @membership = Membership.find(params[:team_id])
-    @membership.user = current_user
+    @team = Team.find(params[:team_id])
+    @membership = Membership.new
     @membership.team = @team
-    if membership.save
+    @membership.user = current_user
+    if @membership.save
+      redirect_to team_path(@team)
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    @membership = Membership.find(params[:id])
+    @membership.destroy
+    redirect_to user_path(current_user)
   end
 
 end
