09ebe9914d14a568354cd3f1ce5f34ec181e97b1
Alexander Stewart <ac_stewart@icloud.com>
2020-03-10 17:52:38 +0000

    homepage styling updated and map marker progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
+  require 'json'
+
   def home
+    @games = Game.geocoded
+    @markers = @games.map do |game|
+      {
+        lat: game.latitude,
+        lng: game.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { game: game }),
+        image_url: helpers.asset_url(set_markers(game))
+      }
+      end
   end
 end
