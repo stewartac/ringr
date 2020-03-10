ce9bdcc1e0646c8b2fe590e378bfd70274924048
Alexander Stewart <ac_stewart@icloud.com>
2020-03-10 12:36:34 +0000

    initial work on map reload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,23 +1,22 @@
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("@rails/ujs").start()
+require("turbolinks").start()
 
 import "bootstrap";
 
-import { initMapbox } from '../plugins/init_mapbox';
+import { mapBox } from '../plugins/init_mapbox';
 
-window.addEventListener('load', () => {
-  initMapbox();
-})
-
-
-const checkboxInputs = document.querySelectorAll('.form-check-input');
-
-checkboxInputs.forEach((input) => {
-  input.addEventListener('click', () => {
-    input.classList.toggle('active');
+document.addEventListener('turbolinks:load', () => {
+  mapBox();
+  const checkboxInputs = document.querySelectorAll('.form-check-input');
+  checkboxInputs.forEach((input) => {
+    input.addEventListener('click', () => {
+      input.classList.toggle('active');
+    })
   })
+
+  const iconClick = document.querySelectorAll('.form-check-input');
 })
 
-const iconClick = document.querySelectorAll('.form-check-input');
+
