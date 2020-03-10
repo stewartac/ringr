d2c76b663425abaf8447ae180d4e4936fef825b0
Alexander Stewart <ac_stewart@icloud.com>
2020-03-10 12:52:53 +0000

    turbolinks taken out so that map works on game show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,16 @@
 require("@rails/activestorage").start()
 require("channels")
 require("@rails/ujs").start()
-require("turbolinks").start()
 
 import "bootstrap";
 
 import { mapBox } from '../plugins/init_mapbox';
 
-document.addEventListener('turbolinks:load', () => {
-  mapBox();
-  const checkboxInputs = document.querySelectorAll('.form-check-input');
-  checkboxInputs.forEach((input) => {
-    input.addEventListener('click', () => {
-      input.classList.toggle('active');
-    })
+mapBox();
+
+const checkboxInputs = document.querySelectorAll('.form-check-input');
+checkboxInputs.forEach((input) => {
+  input.addEventListener('click', () => {
+    input.classList.toggle('active');
   })
-
-  const iconClick = document.querySelectorAll('.form-check-input');
 })
-
-
