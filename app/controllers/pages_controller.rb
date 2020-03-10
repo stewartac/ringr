f19d83b3486e0e03db995d0aeb9d08d0ae377a8a
Alexander Stewart <ac_stewart@icloud.com>
2020-03-10 17:53:49 +0000

    map on homepage working with pins

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,27 @@ class PagesController < ApplicationController
       {
         lat: game.latitude,
         lng: game.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { game: game }),
         image_url: helpers.asset_url(set_markers(game))
       }
       end
+  end
+
+  private
+
+   def set_markers(game)
+   placeholder = ''
+   if game.sport == "Football"
+    placeholder = 'football-marker.png'
+   elsif game.sport == "Basketball"
+    placeholder = 'basketball-marker.png'
+   elsif game.sport == "Netball"
+    placeholder = 'netball-marker.png'
+   elsif game.sport == 'Rugby'
+    placeholder = 'rugby-marker.png'
+   elsif game.sport == "Cricket"
+    placeholder = 'cricket-marker.png'
+   elsif game.sport == "Hockey"
+    placeholder = 'hockey-marker.png'
+    end
   end
 end
