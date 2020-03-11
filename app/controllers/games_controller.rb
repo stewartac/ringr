cdc00608b12bb23d568400834f11628e31c16239
Alexander Stewart <ac_stewart@icloud.com>
2020-03-11 17:37:25 +0000

    filtered search working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,22 +3,26 @@ class GamesController < ApplicationController
   before_action :set_game, only: [:edit, :show, :destroy]
 
   def index
-
+    # raise
     @games = Game.geocoded
     @user = User.geocoded
     if params[:filter]
       array = params.require(:filter).require(:sports)
       array.reject! {|string| string == ""}
-      @games = @games.where(sport: array)
-    elsif params[:hidden_filter]
-      @games = @games.where(sport: params[:hidden_filter].split(" "))
+      @games = @games.where(sport:array)
+    elsif params[:filter2]
+      # raise
+      sports = params.require(:filter2).gsub("[", "").gsub("]","").gsub('"','').gsub(" ","").split(",")
+      @games = @games.where(sport:sports)
+    # elsif params[:hidden_filter]
+    #   @games = @games.where(sport: params[:hidden_filter].split(" "))
     end
 
-    # @addresses = @games.pluck(:address)
-    # if params[:address].present?
-    #   @games = @games.search_by_address(params[:address])
-    #   @games.to_a.reject! { |g| g.latitude.nil? || g.longitude.nil?}
-    # end
+    @addresses = @games.pluck(:address)
+    if params[:address].present?
+      @games = @games.search_by_address(params[:address])
+      @games.to_a.reject! { |g| g.latitude.nil? || g.longitude.nil?}
+    end
     @markers = @games.map do |game|
       {
         lat: game.latitude,
@@ -33,6 +37,7 @@ class GamesController < ApplicationController
     elsif params[:sort_by] == "time"
       @games = @games.sort_by{|game| game.time}
     end
+
   end
 
   def show
